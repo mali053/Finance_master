28f669fe15fde4646188476821a0e7cabcdb3562
mali053 <m9743740@gmail.com>
2024-05-22 01:36:37 +0300

    add vizualization routing and add all the needed routing to the main

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
 from app.controllers.user_controller import user_router
+from app.controllers.expense_controller import expense_router
+from app.controllers.revenue_controller import revenue_router
+from app.controllers.vizualization_controller import visualization_router
 
 app = FastAPI()
 
 app.include_router(user_router, prefix='/user')
+app.include_router(expense_router, prefix='/expense')
+app.include_router(revenue_router, prefix='/revenue')
+app.include_router(visualization_router, prefix='/vis')
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
